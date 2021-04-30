34a7855e7eab0adfd9906f254b12fb4d4cb2a959
Hans Ren <845231@swansea.ac.uk>
2021-04-30 17:40:55 +0100

    Create __init__.py

---
+++ Backbone/__init__.py
@@ -0,0 +1,4 @@
+# -*-coding:utf-8-*-
+from Backbone.lenet import LeNet
+from Backbone.resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
+from Backbone.densenet import DenseNet121, DenseNet169, DenseNet201, DenseNet161
